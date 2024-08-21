a2a583c43718badbfbec1462c9b8f7d9aab7a256
Matthias <mattwi@fysik.dtu.dk>
2024-08-21 17:02:53 +0200

    First perp and para advection implementation

--- src/thermal/parallel.h
+++ src/thermal/parallel.h
@@ -44,13 +44,43 @@ class ParallelDynamics
     const Container& get_apar() const{ return m_apar;}
 
     // call once per species
-    void update_quantities( unsigned s, const Container& aparST, const std::array<std::vector<Container>,6>& y);
+    void update_quantities(
+        unsigned s,
+        const Container& aparST,
+        const std::array<Container,4>& psi,
+        const std::array<std::vector<Container>,6>& y);
     // N, Tperp, Tpara, U, Uperp, Upara
     const std::array<Container,6>& get_q( ) const{return m_q;}
     const std::array<Container,6>& get_qST( ) const{return m_qST;}
+    const std::array<Container,6>& get_psiST( ) const{return m_psiST;}
+
+    void add_para_density_dynamics( unsigned s,
+        std::array<std::vector<Container>,6>& yp);
 
     private:
+    // could be a free function?
+    void compute_parallel_flux( const Container& velocity,
+             const Container& minusST, const Container& plusST,
+             Container& flux,
+             )
+    {
+        if( m_reversed_field)
+        {
+            // Upwind( -v, minusST, plusST) == Upwind( v, plusST, minusST)
+            dg::blas1::evaluate( flux, dg::equals(), dg::Upwind(),
+                velocity, plusST, minusST);
+        }
+        else
+        {
+            dg::blas1::evaluate( flux, dg::equals(), dg::Upwind(),
+                velocity, minusST, plusST);
+        }
+        dg::blas1::pointwiseDot( velocity, flux, flux);
+    }
     dg::geo::Fieldaligned<Geometry, IMatrix, Container> m_fa, m_faST;
+
+    Container m_temp, m_apar, m_tminus, m_tplus;
+    Container m_divb;
 
     std::vector<Container> m_minusSTN, m_STN;
     // m_plusSTN = 2*m_STN - m_minusSTN
@@ -59,9 +89,12 @@ class ParallelDynamics
     // N, Tperp, Tpara, U, Uperp, Upara
     std::array<Container,6> m_minus, m_zero, m_plus;
     std::array<Container,6> m_minusST, m_plusST, m_q, m_qST;
+    // ds Psi
+    std::array<Container,2> m_dsP, m_dsPST;
+    // PsiST
+    std::array<Container,4> m_psiST;
 
-    std::array<Container,4> m_minusSTGamma, m_plusSTGamma;
-    Container m_temp, m_apar, m_gradParGamma2;
+    std::array<Container,6> m_divNUb;
 
     const thermal::Parameters m_p;
     const dg::file::WrappedJsonValue m_js;
@@ -75,6 +108,8 @@ ParallelDynamics<Grid, IMatrix, Matrix, Container>::ParallelDynamics( const Grid
     ): m_p(p), m_js(js)
 {
     dg::assign( dg::evaluate( dg::zero, g), m_temp );
+    m_tminus = m_tplus = m_apar = m_temp;
+
     m_minusSTN.resize( m_p.num_species);
     std::fill( m_minusSTN.begin(), m_minusSTN.end(), m_temp);
     m_STN = m_minusSTN;
@@ -83,9 +118,10 @@ ParallelDynamics<Grid, IMatrix, Matrix, Container>::ParallelDynamics( const Grid
     std::fill( m_minusST.begin(), m_minusST.end(), m_temp);
     std::fill( m_plusST.begin(), m_plusST.end(), m_temp);
     m_q = m_qST = m_plusST;
+    std::fill( m_dsP.begin(), m_dsP.end(), m_temp);
+    std::fill( m_dsPST.begin(), m_dsPST.end(), m_temp);
 
-    std::fill( m_plusSTGamma.begin(), m_plusSTGamma.end(), m_temp);
-    m_apar = m_gradParGamma2 = m_temp;
+    std::fill( m_divNUb.begin(), m_divNUb.end(), m_temp);
 
     m_reversed_field = false;
     if( mag.ipol()( g.x0(), g.y0()) < 0)
@@ -100,17 +136,40 @@ ParallelDynamics<Grid, IMatrix, Matrix, Container>::ParallelDynamics( const Grid
         m_faST.construct( bhat, g, dg::NEU, dg::NEU, dg::geo::NoLimiter(),
             p.rk4eps, p.mx, p.my, 2.*M_PI/(double)p.Nz/2., p.interpolation_method );
     }
+    dg::assign(  dg::pullback(dg::geo::Divb(mag), g), m_divb);
 }
+
 template<class Grid, class IMatrix, class Matrix, class Container>
 void ParallelDynamics<Grid, IMatrix, Matrix, Container>::update_quantities(
         unsigned s,
         const Container& aparST,
+        const std::array<Container,4>& psi,
         const std::array<std::vector<Container>,6>& y)
 {
 #ifdef MPI_VERSION
     int rank;
     MPI_Comm_rank( MPI_COMM_WORLD, &rank);
 #endif
+    // 0 transform psi
+    for( unsigned u=0; u<4; u++)
+    {
+        if( u==1)
+        { // we only need GradPar Gamma2
+            m_fa( dg::geo::einsMinus, psi[u], m_tminus);
+            m_fa( dg::geo::zeroForw,  psi[u], m_temp);
+            m_fa( dg::geo::einsPlus,  psi[u], m_tplus);
+            update_parallel_bc_2nd( m_fa, m_tminus, m_temp,
+                m_tplus, m_p.bcxP, 0.);
+            dg::geo::ds_centered( m_fa, 1., m_tminus, m_tplus, 0., m_dsP[u]);
+        }
+        m_faST( dg::geo::zeroMinus, psi[u], m_tminus);
+        m_faST( dg::geo::einsPlus,  psi[s], m_tplus);
+        update_parallel_bc_1st( m_tminus, m_tplus,
+                m_p.bcxP, 0.);
+        if( u < 2)
+            dg::geo::ds_centered( m_faST, 1., m_tminus, m_tplus, 0., m_dsPST[u]);
+        dg::blas1::axpby( 0.5, m_tminus, 0.5, m_tplus, m_psiST[u]);
+    }
     // 1st transform all densities
     std::vector<Container>&  density    = y[0];
     std::vector<Container>&  pperp      = y[1];
@@ -165,6 +224,111 @@ void ParallelDynamics<Grid, IMatrix, Matrix, Container>::update_quantities(
         dg::blas1::axpby( 0.5, m_minusST[u], 0.5, m_plusST[u], m_q[u]);
     }
     // Now we have q, qST, qminus, qzero, qplus, qminusST, qplusST
+
 }
+
+template<class Grid, class IMatrix, class Matrix, class Container>
+void ParallelDynamics<Grid, IMatrix, Matrix, Container>::add_para_density_dynamics(
+        unsigned s,
+        const std::array<Container,4>& psi,
+        const std::array<std::vector<Container>,6>& y,
+        std::array<std::vector<Container>,6>& yp)
+{
+    // "velocity-staggered" - "flux-centered"
+    for( unsigned u=0; u<3; u++)
+    {
+        // use divNUb as a temporary ...
+        if( u == 0)
+            dg::blas1::copy( m_zero[3], m_divNUb[u]);
+        else if( u==1)
+            dg::blas1::axpby( 1., m_zero[3], 1., m_zero[4], m_divNUb[u]);
+        else if( u==2)
+            dg::blas1::axpby( 1., m_zero[3], 1., m_zero[5], m_divNUb[u]);
+        compute_parallel_flux( m_divNUb[u], m_minusST[u], m_plusST[u],
+                m_temp, m_p.slope_limiter);
+        m_faST( dg::geo::zeroPlus,  m_temp, m_tplus);
+        m_faST( dg::geo::einsMinus, m_temp, m_tminus);
+        update_parallel_bc_1st( m_tminus, m_tplus, dg::NEU, 0.);
+        dg::geo::ds_divCentered( m_faST, 1., m_tminus, m_tplus, 0., m_divNUb[u]);
+        dg::blas1::axpby( -1., m_divNUb[u], 1., yp[u][s]);
+    }
+    // -2P_para GradPar U
+    dg::geo::ds_centered( m_faST, 1., m_minusST[3], m_plusST[3],0., m_temp);
+    dg::blas1::pointwiseDot( -2., y[2][s], m_temp, 1., yp[2][s]);
+    // -2z N U_perp E_1,para
+    dg::geo::ds_centered( m_fa, 1., m_minus[1], m_plus[1], 0., m_temp); //GradPar Tperp
+    dg::blas1::pointwiseDivide( m_temp, m_q[1], m_temp);
+    dg::blas1::axpby(1., m_divb, 1., m_temp);
+    dg::blas1::pointwiseDot( -1., psi[2], m_temp, +1., psi[1], m_temp, 0., m_temp);
+    dg::blas1::axpby( 1., m_dsP[1], 1., m_temp);
+    dg::blas1::pointwiseDot( -2.*m_p.z[s], m_q[0], m_q[4], m_temp, 1., yp[2][s]);
+}
+
+template<class Grid, class IMatrix, class Matrix, class Container>
+void ParallelDynamics<Grid, IMatrix, Matrix, Container>::add_para_velocity_dynamics(
+        unsigned s,
+        const std::array<std::vector<Container>,6>& y,
+        std::array<std::vector<Container>,6>& yp)
+{
+    // "velocity-staggered" - "flux-centered"
+    for( unsigned u=3; u<6; u++)
+    {
+        compute_parallel_flux( m_q[3], m_minusST[u], m_plusST[u],
+                m_temp, m_p.slope_limiter);
+        m_faST( dg::geo::zeroPlus,  m_temp, m_tplus);
+        m_faST( dg::geo::einsMinus, m_temp, m_tminus);
+        update_parallel_bc_1st( m_tminus, m_tplus, dg::NEU, 0.);
+        if( u==3)
+            dg::geo::ds_centered( m_faST, -0.5, m_tminus, m_tplus, 0, m_divNUb[3]);
+        else
+            dg::geo::ds_divCentered( m_faST, 1., m_tminus, m_tplus, 0., m_divNUb[u]);
+        dg::blas1::axpby( -1., m_divNUb[u], 1., yp[u][s]);
+    }
+    dg::geo::ds_centered( m_faST, 1., m_minusST[1], m_plusST[1],0., m_temp); // dsTperp
+    dg::blas1::pointwiseDot( -1./m_p.mu[s], m_qST[2], m_qST[0], m_temp, 1., yp[4][s]);
+
+    dg::geo::ds_centered( m_faST, 1., m_minusST[2], m_plusST[2],0., m_temp); // dsTpara
+    dg::blas1::axpby( -1./m_p.mu[s], m_temp, 1., yp[3][s]);
+    dg::blas1::pointwiseDot( -3./m_p.mu[s], m_qST[2], m_qST[0], m_temp, 1., yp[5][s]);
+
+    dg::geo::ds_centered( m_fa, 1., m_minus[3], m_plus[3],0., m_temp); //dsU
+    dg::blas1::pointwiseDot( -1., y[4][s], m_temp, 1., yp[4][s]);
+    dg::blas1::pointwiseDot( -3., y[5][s], m_temp, 1., yp[5][s]);
+
+    // Add density gradient and electric field
+    double z = m_p.z[s], mu = m_p.mu[s], delta = m_fa.deltaPhi();
+    dg::blas1::subroutine( [z, mu, delta ]DG_DEVICE (
+    double& WDot, double& QperpDot,
+        double N, double Tperp, double Tpara,
+        double QN, double PN,
+        double QTperp, double PTperp,
+        double dsG1, double dsG2,
+        double G2, double G3, double divb
+                )
+        {
+            gradParLnN = bphi*(PN-QN)/delta/2.*(1/PN + 1/QN);
+            gradParLnTperp = bphi*(PTperp-QTperp)/delta/2.*(1./PTperp + 1/QTperp);
+            WDot     -= z/mu*(dsG1 - G2*(gradParLnTperp + divb));
+            QperpDot -= z/mu*N*Tperp*(dsG2 - (G3-G2)*(gradParLnTperp + divb));
+            WDot -= Tpara/mu*gradParLnN;
+        },
+        yp[3][s], yp[4][s],
+        m_qST[0], m_qST[1], m_qST[2],
+        m_minusST[0], m_plusST[0],
+        m_minusST[1], m_plusST[1],
+        m_dsPST[0], m_dsPST[1],
+        m_psiST[1], m_psiST[2], m_divb
+    );
+
+    // -z/mu N T_perp E_1,para
+    dg::geo::ds_centered( m_faST, 1., m_minusST[1], m_plusST[1], 0., m_temp); //GradPar Tperp
+    dg::blas1::pointwiseDivide( m_temp, m_qST[1], m_temp);
+    dg::blas1::axpby(1., m_divb, 1., m_temp);
+    dg::blas1::pointwiseDot( -1., m_psiST[2], m_temp, +1., m_psiST[1], m_temp, 0., m_temp);
+    dg::blas1::axpby( 1., m_dsPST[1], 1., m_temp);
+    dg::blas1::pointwiseDot( -m_p.z[s]/m_p.mu[s], m_qST[0], m_qST[1], m_temp, 1., yp[4][s]);
+
+}
+
 
 }//namespace thermal
--- src/thermal/perpendicular.h
+++ src/thermal/perpendicular.h
@@ -16,7 +16,20 @@ class PerpDynamics
     PerpDynamics( const Geometry&, thermal::Parameters,
         dg::geo::TokamakMagneticField, dg::file::WrappedJsonValue);
 
+    void update_density_derivatives(
+        unsigned s,
+        const Container& apar,
+        const std::array<std::vector<Container>,6> >& y,
+        const std::array<Container,6>& q
+    );
     void add_perp_density_dynamics(
+        unsigned s,
+        const Container& apar,
+        const std::array<std::vector<Container>,6> >& y,
+        const std::array<Container,6>& q,
+        std::array<std::vector<Container>,6>& yp
+    );
+    void add_perp_velocity_dynamics(
         unsigned s,
         const Container& apar,
         const std::array<std::vector<Container>,6> >& y,
@@ -26,7 +39,7 @@ class PerpDynamics
     private:
     //these should be considered const
     std::array<Container,2> m_curvNabla, m_curvKappa, m_gradLnB;
-    Container m_divCurvKappa, m_bphiB, m_divb; //m_bphiB is bhat/ sqrt(g) / B (covariant component)
+    Container m_divCurvKappa, m_b_2, m_divb; //m_bphiB is bhat/ sqrt(g) / B (covariant component)
 
     Matrix m_dxF_N, m_dxB_N, m_dxF_U, m_dxB_U, m_dx_N, m_dx_U, m_dx_P, m_dx_A;
     Matrix m_dyF_N, m_dyB_N, m_dyF_U, m_dyB_U, m_dy_N, m_dy_U, m_dy_P, m_dy_A;
@@ -109,19 +122,19 @@ PerpDynamics<Grid, IMatrix, Matrix, Container>::PerpDynamics( const Grid& g,
         throw std::runtime_error( "curvmode : true is not possible in thermal code!");
     else if( reversed_field)
         bhat = dg::geo::createEPhi(-1);
-    dg::pushForward(bhat.x(), bhat.y(), bhat.z(), m_temp0, m_temp1, m_bphiB, g);
+    dg::pushForward(bhat.x(), bhat.y(), bhat.z(), m_temp0, m_temp1, m_b_2, g);
     // make bhat covariant:
-    dg::tensor::inv_multiply3d( g.metric(), m_temp0, m_temp1, m_bphiB,
-                                            m_temp0, m_temp1, m_bphiB);
+    dg::tensor::inv_multiply3d( g.metric(), m_temp0, m_temp1, m_b_2,
+                                            m_temp0, m_temp1, m_b_2);
     // Grad Ln B covariant components
     m_gradLnB[0] = dg::pullback( dg::geo::BR(mag), g);
     m_gradLnB[1] = dg::pullback( dg::geo::BZ(mag), g);
     m_temp0 = dg::pullback(dg::geo::InvB(mag), g);
     dg::blas1::pointwiseDot( m_gradLnB[0], m_temp0, m_gradLnB[0]);
     dg::blas1::pointwiseDot( m_gradLnB[1], m_temp0, m_gradLnB[1]);
-    dg::blas1::pointwiseDot( m_bphiB, m_temp0, m_bphiB); // bphi / B
+    dg::blas1::pointwiseDot( m_b_2, m_temp0, m_bphiB); // bphi / B
     m_temp0 = dg::tensor::volume( metric);
-    dg::blas1::pointwiseDivide( m_bphiB, m_temp0, m_bbphiB); //b_2/detg/B
+    dg::blas1::pointwiseDivide( m_b_2, m_temp0, m_bbphiB); //bphiB/detg/B
 
     // allocate resources for internal vectors
     std::fill( m_dxF.begin(), m_dxF.end(), m_temp0);
@@ -129,17 +142,17 @@ PerpDynamics<Grid, IMatrix, Matrix, Container>::PerpDynamics( const Grid& g,
     std::fill( m_dx.begin(), m_dx.end(), m_temp0);
     std::fill( m_dy.begin(), m_dy.end(), m_temp0);
 }
+
 template<class Grid, class IMatrix, class Matrix, class Container>
-void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
+void PerpDynamics<Grid, IMatrix, Matrix, Container>::update_density_derivatives(
     unsigned s,
     const Container& apar,
     const std::array<Container,4>& psi,
     const std::array<std::vector<Container>,6> >& y,
-    const std::array<Container,6>& q,
-    std::array<std::vector<Container>,6>& yp
+    const std::array<Container,6>& q
 )
 {
-    const Container& to_advect[3] = {y[s][0], y[s][1], y[s][2]};
+    const Container& to_advect[3] = {y[0][s], y[1][s], y[2][s]};
     double nbc[3] = {m_p.nbc[s], m_p.nbc[s]*m_p.tbc, m_p.nbc[s]*m_p.tbc};
     for( unsigned u=0; u<3; u++)
     {
@@ -163,11 +176,23 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
         dg::blas2::symv( m_dx_U, q[u], m_dx[u]);
         dg::blas2::symv( m_dy_U, q[u], m_dy[u]);
     }
-    for( unsigned u=6; u<9; u++)
+    for( unsigned u=0; u<3; u++)
     {
-        dg::blas2::symv( m_dx_P, q[u], m_dx[u]);
-        dg::blas2::symv( m_dy_P, q[u], m_dy[u]);
+        dg::blas2::symv( m_dx_P, psi[u], m_dx[6+u]);
+        dg::blas2::symv( m_dy_P, psi[u], m_dy[6+u]);
     }
+}
+
+template<class Grid, class IMatrix, class Matrix, class Container>
+void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
+    unsigned s,
+    const Container& apar,
+    const std::array<Container,4>& psi,
+    const std::array<std::vector<Container>,6> >& y,
+    const std::array<Container,6>& q,
+    std::array<std::vector<Container>,6>& yp
+)
+{
     /////////////////////////////////////////////////////////////////
     double mu = m_p.mu[s], z = m_p.z[s], beta = m_p.beta;
     dg::blas1::subroutine( [mu, z, beta] DG_DEVICE (
@@ -190,7 +215,7 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
             double curvNablaX, double curvNablaY,
             double curvKappaX, double curvKappaY,
             double gradLnBX, double gradLnBY,
-            double divCurvKappa, double bphiB, double divb,
+            double divCurvKappa, double b_2, double divb,
             double& dtN, double& dtPperp, double& dtPpara
         )
     {
@@ -201,8 +226,8 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
         double bpX = 0., bpY = 0., divbp = 0.;
         if( beta != 0)
         {
-            bpX = A * curvKappaX + (   dyA*bphiB);
-            bpY = A * curvKappaY + ( - dxA*bphiB);
+            bpX = A * curvKappaX + (   dyA*b_2);
+            bpY = A * curvKappaY + ( - dxA*b_2);
             divbp = A*divCurvKappa - curvNablaX*dxA - curvNablaY*dyA;
         }
         double vX = U * bpX + ( - b_2*E0Y) + Tperp/z *curvNablaX + (Tpara + mu*U*U)/z*curvKappaX;
@@ -244,11 +269,11 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
 
         double divuE0 =  (curvNablaX+curvKappaX)*E0X
                         +(curvNablaY+curvKappaY)*E0Y
-                        +bphiB* ( dxG2*(dyTperp/Tperp - gradLnBY)
+                        +b_2* ( dxG2*(dyTperp/Tperp - gradLnBY)
                                  -dyG2*(dxTperp/Tperp - gradLnBX));
         double divuE1 =  (curvNablaX+curvKappaX)*E1X
                         +(curvNablaY+curvKappaY)*E1Y
-                        +bphiB* ( (dxG3-dxG2)*(dyTperp/Tperp - gradLnBY)
+                        +b_2* ( (dxG3-dxG2)*(dyTperp/Tperp - gradLnBY)
                                  -(dyG3-dyG2)*(dxTperp/Tperp - gradLnBX));
         dtN     -= N*divuE0;
         dtPperp -= N*Tperp*(divuE0 + divuE1);
@@ -267,13 +292,13 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
         dtPara -=2*N*(bpX*Tpara
                     + mu/z*(Tpara*Upara + 2*U*Tpara)*curvKappaX
                     + mu/z*Tperp*Uperp*curvNablaX
-                    -mu*Uperp*bphiB*E1Y)*dxU;
+                    -mu*Uperp*b_2*E1Y)*dxU;
         dtPara -=2*N*(bpY*Tpara
                     + mu/z*(Tpara*Upara + 2*U*Tpara)*curvKappaY
                     + mu/z*Tperp*Uperp*curvNablaY
-                    +mu*Uperp*bphiB*E1X)*dyU;
+                    +mu*Uperp*b_2*E1X)*dyU;
     },
-    to_advect[0],
+    y[0][s],
     m_dxF[0], m_dyF[0],
     m_dxB[0], m_dyB[0],
     m_dxF[1], m_dyF[1],
@@ -292,10 +317,192 @@ void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_density_dynamics(
     m_curvNabla[0], m_curvNabla[1],
     m_curvKappa[0], m_curvKappa[1],
     m_gradLnB[0], m_gradLnB[1],
-    m_divCurvKappa, m_bphiB, m_divb,
-    yp[0], yp[1], yp[2]
+    m_divCurvKappa, m_b_2, m_divb,
+    yp[0][s], yp[1][s], yp[2][s]
     );
 }
 
+template<class Grid, class IMatrix, class Matrix, class Container>
+void PerpDynamics<Grid, IMatrix, Matrix, Container>::add_perp_velocity_dynamics(
+    unsigned s,
+    const Container& aparST,
+    const std::array<Container,4>& psiST,
+    const std::array<std::vector<Container>,6> >& y,
+    const std::array<Container,6>& qST,
+    std::array<std::vector<Container>,6>& yp
+)
+{
+    const Container& to_advect[3] = {qST[3], y[4][s], y[5][s]};
+    for( unsigned u=0; u<3; u++)
+    {
+        dg::blas2::symv( m_dxF_N, to_advect[u], m_dxF[u]);
+        dg::blas2::symv( m_dxB_N, to_advect[u], m_dxB[u]);
+        dg::blas2::symv( m_dyF_N, to_advect[u], m_dyF[u]);
+        dg::blas2::symv( m_dyB_N, to_advect[u], m_dyB[u]);
+    }
+    for( unsigned u=0; u<3; u++)
+    {
+        dg::blas1::transform( qST[u], m_temp0, dg::PLUS<double>(u == 0 ? -m_p.nbc[s] : -m_p.tbc));
+        dg::blas2::symv( m_dx_N, m_temp0, m_dx[u]);
+        dg::blas2::symv( m_dy_N, m_temp0, m_dy[u]);
+    }
+    dg::blas2::symv( m_dx_A, aparST, m_dx[3]);
+    dg::blas2::symv( m_dy_A, aparST, m_dy[3]);
+    for( unsigned u=0; u<4; u++)
+    {
+        dg::blas2::symv( m_dx_P, psiST[u], m_dx[4+u]);
+        dg::blas2::symv( m_dy_P, psiST[u], m_dy[4+u]);
+    }
+    double mu = m_p.mu[s], z = m_p.z[s], beta = m_p.beta;
+    dg::blas1::subroutine( [mu, z, beta] DG_DEVICE (
+            double U,
+            double dxFU, double dyFU,
+            double dxBU, double dyBU,
+            double dxFQperp, double dyFQperp,
+            double dxBQperp, double dyBQperp,
+            double dxFQpara, double dyFQpara,
+            double dxBQpara, double dyBQpara,
+            double N, double dxN, double dyN,
+            double Tperp, double dxTperp, double dyTperp,
+            double Tpara, double dxTpara, double dyTpara,
+            double A, double dxA, double dyA,
+            double Uperp, double Upara,
+                       double dxG1, double dyG1,
+            double G2, double dxG2, double dyG2,
+            double G3, double dxG3, double dyG3,
+            double G4, double dxG4, double dyG4,
+            double curvNablaX, double curvNablaY,
+            double curvKappaX, double curvKappaY,
+            double gradLnBX, double gradLnBY,
+            double divCurvKappa, double b_2, double divb,
+            double& dtU, double& dtQperp, double& dtQpara
+        )
+    {
+        double E0X = dxG1 - G2*(dxTperp/Tperp - gradLnBX);
+        double E0Y = dyG1 - G2*(dyTperp/Tperp - gradLnBY);
+        double E1X = dxG2 - (G3-G2)*(dxTperp/Tperp - gradLnBX);
+        double E1Y = dyG2 - (G3-G2)*(dyTperp/Tperp - gradLnBY);
+        double E2X = dxG3 - (G4-2*G3)*(dxTperp/Tperp - gradLnBX);
+        double E2Y = dyG3 - (G4-2*G3)*(dyTperp/Tperp - gradLnBY);
+        double bpX = 0., bpY = 0., divbp = 0.;
+        if( beta != 0)
+        {
+            bpX = A * curvKappaX + (   dyA*b_2);
+            bpY = A * curvKappaY + ( - dxA*b_2);
+            divbp = A*divCurvKappa - curvNablaX*dxA - curvNablaY*dyA;
+        }
+        // U
+        double vX = U * bpX + ( - b_2*E0Y) + Tperp/z *curvNablaX + (3*Tpara + mu*U*U)/z*curvKappaX;
+        double vY = U * bpY + (   b_2*E0X) + Tperp/z *curvNablaY + (3*Tpara + mu*U*U)/z*curvKappaY;
+        dtU += ( vX > 0 ) ? -vX*dxBU : -vX*dxFU;
+        dtU += ( vY > 0 ) ? -vY*dyBU : -vY*dyFU;
+        // Qperp
+        vX = U * bpX + ( - b_2*(E0Y+E2Y)) + 3*Tperp/z *curvNablaX + (Tpara + mu*U*U)/z*curvKappaX;
+        vY = U * bpY + (   b_2*(E0X+E2X)) + 3*Tperp/z *curvNablaY + (Tpara + mu*U*U)/z*curvKappaY;
+        dtQperp += ( vX > 0 ) ? -vX*dxBQperp : -vX*dxFQperp;
+        dtQperp += ( vY > 0 ) ? -vY*dyBQperp : -vY*dyFQperp;
+        // Qpara
+        vX = U * bpX + ( - b_2*E0Y) + Tperp/z *curvNablaX + (7*Tpara + mu*U*U)/z*curvKappaX;
+        vY = U * bpY + (   b_2*E0X) + Tperp/z *curvNablaY + (7*Tpara + mu*U*U)/z*curvKappaY;
+        dtQpara += ( vX > 0 ) ? -vX*dxBQpara : -vX*dxFQpara;
+        dtQpara += ( vY > 0 ) ? -vY*dyBQpara : -vY*dyFQpara;
+
+        dtU -= 2/z*( U*Tpara*divCurvKappa + U * (curvKappaX*dxTpara + curvKappaY*dyTpara)
+                        + U*Tpara/N *(curvKappaX*dxN + curvKappaY*dyN))
+
+        double divuE1 =  (curvNablaX+curvKappaX)*E1X
+                        +(curvNablaY+curvKappaY)*E1Y
+                        +b_2* ( (dxG3-dxG2)*(dyTperp/Tperp - gradLnBY)
+                                 -(dyG3-dyG2)*(dxTperp/Tperp - gradLnBX));
+        dtU -=    Tpara/mu*(divb+divbp)
+                + 1./mu*(bpX*dxTpara + bpY*dyTpara)
+                + Tpara/mu/N*(bpX*dxN + bpY*dyN)
+                + Tpara*Upara/z*divCurvKappa
+                + 1./z/N*( curvKappaX * dxFQpara + curvKappaY * dyFQpara)
+                - Tperp/z*Uperp*divCurvKappa
+                + 1/z/N*( curvNablaX * dxFQperp + curvNablaY * dyFQperp)
+                + Uperp*divuE1
+                + 1./N/Tperp*b_2*(E1X*dyFQperp - E1Y*dxFQperp)
+                - Uperp/Tperp*b_2*(E1X*dyTperp - E1Y*dxTperp);
+        dtU += Tperp/mu*(div+divbp)
+                + Tperp/z*(Uperp+U)*divCurvKappa
+                - z/mu*(bpX*E0X+bpY*E0Y)
+                - U*(    curvKappaX*E0X + curvKappaY*E0Y)
+                - Uperp*(curvKappaX*E1X + curvKappaY*E1Y);
+
+        double divuE0 =  (curvNablaX+curvKappaX)*E0X
+                        +(curvNablaY+curvKappaY)*E0Y
+                        +b_2* ( dxG2*(dyTperp/Tperp - gradLnBY)
+                                 -dyG2*(dxTperp/Tperp - gradLnBX));
+        double divuE2 =  (curvNablaX+curvKappaX)*E2X
+                        +(curvNablaY+curvKappaY)*E2Y
+                        +b_2* ( (dxG4-2*dxG3)*(dyTperp/Tperp - gradLnBY)
+                                 -(dyG4-2*dyG3)*(dxTperp/Tperp - gradLnBX));
+        dtQperp -= N*Tperp*Uperp*(
+                        U*divbp + bpX*dxU + bpY*dyU
+                        +1/z*(3*Tpara + mu*U*U)*divCurvKappa
+                        +1/z*(3*(curvKappaX*dxTpara+curvKappaY*dyTpara)+2*mu*U*(curvKappaX*dxU+curvKappaY*dyU))
+                        -3/z*Tperp*divCurvKappa
+                        +3/z*(curvNablaX*dxTperp + curvNablaY*dyTperp)
+                        +divuE0+divuE2);
+        dtQpara -= N*Tpara*Upara*(
+                        U*divbp + bpX*dxU + bpY*dyU
+                        +1/z*(7*Tpara + mu*U*U)*divCurvKappa
+                        +1/z*(7*(curvKappaX*dxTpara+curvKappaY*dyTpara)+2*mu*U*(curvKappaX*dxU+curvKappaY*dyU))
+                        -1/z*Tperp*divCurvKappa
+                        +1/z*(curvNablaX*dxTperp + curvNablaY*dyTperp)
+                        +divuE0);
+        // temperature transfer
+        vX = N*Tpara/mu*bpX + 1/z*N*Tpara*(Upara+2*mu*U)*curvKappaX + 1/z*N*Tperp*Uperp*curvNablaX
+            +N*UPerp*( -b_2*E1Y);
+        vY = N*Tpara/mu*bpY + 1/z*N*Tpara*(Upara+2*mu*U)*curvKappaY + 1/z*N*Tperp*Uperp*curvNablaY
+            +N*UPerp*(  b_2*E1X);
+        dtQperp -= vX*dxTperp + vY*dyTperp;
+        dtQpara -= 3*(vX*dxTpara + vY*dyTpara);
+        // velocity transfer
+        dtQperp -= (N*Tperp*Uperp*bpX + 2*mu/z*U*N*Tperp*Uperp*curvKappaX + 1/z*N*Tperp*Tperp*curvNablaX
+            +N*TPerp*( -b_2*E1Y))*dxU;
+        dtQperp -= (N*Tperp*Uperp*bpY + 2*mu/z*U*N*Tperp*Uperp*curvKappaY + 1/z*N*Tperp*Tperp*curvNablaY
+            +N*TPerp*(  b_2*E1X))*dyU;
+
+        dtQpara -= (N*Tpara*Upara*bpX + 2/z*(N*Tpara*Tpara + mu*U*N*Tpara*Upara)*curvKappaX)*dxU;
+        dtQpara -= (N*Tpara*Upara*bpY + 2/z*(N*Tpara*Tpara + mu*U*N*Tpara*Upara)*curvKappaY)*dyU;
+
+        // Force terms
+        dtQperp +=   (N*Tperp*(Tperp-Tpara)/mu-N*Tperp*Uperp*U)*(divb+divbp)
+                    +1/z * ( 3*N*Tperp*Uperp*(Tperp-Tpara) + N*Tperp*U*(Tperp - 2*Tpara)
+                            -Tperp*N*Tpara*Upara - mu*N*Tperp*Uperp*U*U ) * divCurvKappa
+                    -(z/mu*N*Tperp*(bpX*E1X+bpY*E1Y)
+                        +N*Tperp*Uperp*(curvKappaX*(E0X+E2X)+curvKappaY*(E0Y+E2Y))
+                        +N*Tperp*U    *(curvKappaX*E1X + curvKappaY*E1Y)
+                        +N*Tperp*Uperp*(curvNablaX*(E0X+E1X+E2X) + curvNablaY*(E0Y+E1Y+E2Y)));
+        dtQpara += 3*( (2*Tpara/z*N*Tperp*Uperp + Tperp/z*N*Tpara*Upara) * divCurvKappa
+                        -N*Tpara*Upara*(curvKappaX*E0X + curvKappaY*E0Y)
+                        -2*N*Tpara*Uperp*(curvKappaX*E1X + curvKappaY*E1Y));
+
+    }
+    qST[3],
+    m_dxF[0], m_dyF[0],
+    m_dxB[0], m_dyB[0],
+    m_dxF[1], m_dyF[1],
+    m_dxB[1], m_dyB[1],
+    m_dxF[2], m_dyF[2],
+    m_dxB[2], m_dyB[2],
+    qST[0], m_dx[0], m_dy[0],
+    qST[1], m_dx[1], m_dy[1],
+    qST[2], m_dx[2], m_dy[2],
+    aparST, m_dx[3], m_dy[3],
+    qST[4], qST[5],
+        m_dx[4], m_dy[4],
+    psiST[1], m_dx[5], m_dy[5],
+    psiST[2], m_dx[6], m_dy[6],
+    psiST[3], m_dx[7], m_dy[7],
+    m_curvNabla[0], m_curvNabla[1],
+    m_curvKappa[0], m_curvKappa[1],
+    m_gradLnB[0], m_gradLnB[1],
+    m_divCurvKappa, m_b_2, m_divb,
+    yp[3][s], yp[4][s], yp[5][s]
+    );
+}
 } //namespace thermal
 
